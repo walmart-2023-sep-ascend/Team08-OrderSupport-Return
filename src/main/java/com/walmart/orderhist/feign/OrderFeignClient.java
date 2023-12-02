7170d2792569acf8e9faf3ef09bc06d3681b87b9
s0p05qj <sathishkumar.ph@walmart.com>
2023-12-02 22:10:58 +0530

    added fall back

--- src/main/java/com/walmart/orderhist/feign/OrderFeignClient.java
+++ src/main/java/com/walmart/orderhist/feign/OrderFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.walmart.orderhist.dto.OrderResponse;
 import com.walmart.orderhist.exception.OrderServiceException;
 
-@FeignClient(name = "order-service", url = "https://ascend-team08.free.beeceptor1.com/order")
+@FeignClient(name = "order-service", url = "https://ascend-team08.free.beeceptor.com/order",fallback = OrderFeignClientFallback.class)
 public interface OrderFeignClient {
 
 	@GetMapping("{userId}")
